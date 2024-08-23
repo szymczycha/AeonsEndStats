e07cb10d185d6d47e412fb97e4bf51fd55f60634
Szymon Konieczny <95142108+szymczycha@users.noreply.github.com>
2024-08-23 14:02:23 +0200

    making entities public

--- app/src/main/java/Model/Game.java
+++ app/src/main/java/Model/Game.java
@@ -1,6 +1,7 @@
 package Model;
 
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 import java.io.Serializable;
@@ -9,18 +10,32 @@ import java.util.List;
 
 @Entity(tableName = "games")
 public class Game {
-    @PrimaryKey(autoGenerate = true)
-    int id;
-    List<Model.Player> Players;
-    String NemesisName;
-    int GraveholdHP;
-    int NemesisHP;
-    Boolean isWon;
-    String ReasonForOutcome;
-    Date date;
-    Boolean HarderVariant;
-    String notes;
 
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public List<Model.Player> Players;
+
+    public String NemesisName;
+
+    public int GraveholdHP;
+
+    public int NemesisHP;
+
+    public Boolean isWon;
+
+    public String ReasonForOutcome;
+
+    public Date date;
+
+    public Boolean HarderVariant;
+
+    public String notes;
+
+    public Game(){
+
+    }
+    @Ignore
     public Game(List<Model.Player> players, String nemesisName, int graveholdHP, int nemesisHP, Boolean isWon, String reasonForOutcome, Date date, Boolean harderVariant, String notes) {
         Players = players;
         NemesisName = nemesisName;
@@ -33,42 +48,52 @@ public class Game {
         this.notes = notes;
     }
 
+    @Ignore
     public List<Model.Player> getPlayers() {
         return Players;
     }
 
+    @Ignore
     public String getNemesisName() {
         return NemesisName;
     }
 
+    @Ignore
     public int getGraveholdHP() {
         return GraveholdHP;
     }
 
+    @Ignore
     public int getNemesisHP() {
         return NemesisHP;
     }
 
+    @Ignore
     public Boolean getWon() {
         return isWon;
     }
 
+    @Ignore
     public String getReasonForOutcome() {
         return ReasonForOutcome;
     }
 
+    @Ignore
     public Boolean getHarderVariant() {
         return HarderVariant;
     }
 
+    @Ignore
     public String getNotes() {
         return notes;
     }
 
+    @Ignore
     public Date getDate() {
         return date;
     }
 
+    @Ignore
     @Override
     public String toString() {
         return "Game{" +
--- app/src/main/java/Model/Player.java
+++ app/src/main/java/Model/Player.java
@@ -1,17 +1,32 @@
 package Model;
 
-public class Player {
-    String mageName;
-    String playerName;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
 
+@Entity
+public class Player {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public String mageName;
+
+    public String playerName;
+
+    public Player(){
+
+    }
+    @Ignore
     public String getMageName() {
         return mageName;
     }
 
+    @Ignore
     public String getPlayerName() {
         return playerName;
     }
 
+    @Ignore
     public Player(String mageName, String playerName) {
         this.mageName = mageName;
         this.playerName = playerName;
